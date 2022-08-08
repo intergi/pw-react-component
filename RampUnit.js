22a91bdf7558fb42bc02d67184ca28e0b0c08e2f
Nico Valdez <nicomvaldez@gmail.com>
2022-08-08 16:42:07 -0300

    converting to npm package

---
+++ RampUnit.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Ramp.css";
+
+export default class Ramp extends React.Component {
+
+  static propTypes = {
+    value: PropTypes.string,
+  };
+
+  render() {
+
+    const props = this.props;
+    window.ramp = window.ramp || {};
+    window.ramp.que = window.ramp.que || [];
+
+    window.ramp.que.push(() => {
+        window.ramp.addUnits([
+          {   
+              selectorId: props.selectorId, 
+              type: props.type
+          }
+          ]).then( () => {
+              window.ramp.displayUnits();
+          }).catch( (e) =>{
+              console.log(e);
+          })
+        }
+    )
+
+    return (
+        <div data-pw-desk={props.type} id={props.selectorId} className={props.cssClass}></div>
+    );
+  }
+}
